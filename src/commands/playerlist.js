e380ea525d474dcf8fead02e07de1c7e9223246b
Ada <71234319+toasterada@users.noreply.github.com>
2023-05-31 17:57:00 +1200

    Update playerlist and serverinfo

--- src/commands/playerlist.js
+++ src/commands/playerlist.js
@@ -2,34 +2,30 @@ const { EmbedBuilder } = require('@discordjs/builders');
 const { SlashCommandBuilder } = require('discord.js');
 const mcping = require('mcping-js');
 
-let status;
-const ip = 'realmplex.toaster.pw';
-
-function timeout() {
-	setTimeout(async () => {
-		const server = new mcping.MinecraftServer(ip, '25569');
-
-		server.ping(10000, 760, (err, res) => {
-			if (err) {
-				console.log(err);
-			} else {
-				status = res.players;
-				if (status.sample == undefined) {
-					status.sample = '*`No players online`*';
-				}
-			}
-		});
-		timeout();
-	}, 10000);
-}
-timeout();
-
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('playerlist')
 		.setDescription('Get a list of players on the server'),
 	async execute(interaction) {
+		const ip = 'realmplex.toaster.pw';
+		const port = '25569';
+		let status;
+		const server = new mcping.MinecraftServer(ip, port);
+
+		await new Promise((resolve, reject) => {
+			server.ping(10000, 760, (err, res) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					status = res.players;
+					if (status.sample == undefined) {
+						status.sample = '*`No players online`*';
+					}
+					resolve();
+				}
+			});
+		});
 		const embed = new EmbedBuilder()
 			.setColor(0xdd50b0)
 			.setTitle('Player List')
--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -1,48 +1,37 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const mcping = require('mcping-js');
 
-let status;
-let buffer;
-const ip = 'realmplex.toaster.pw';
-
-function timeout() {
-	setTimeout(async () => {
-		const server = new mcping.MinecraftServer(ip, '25569');
-
-		server.ping(10000, 760, (err, res) => {
-			if (err) {
-				console.log(err);
-			} else {
-				status = res;
-				const image = status.favicon.replace('data:image/png;base64,', '');
-				buffer = Buffer.from(image, 'base64');
-				if (status.previewsChat == undefined) {
-					status.previewsChat = 'false';
-				}
-				if (status.enforcesSecureChat == undefined) {
-					status.enforcesSecureChat = 'false';
-				}
-				try {
-					if (status.description.text == '') {
-						status.description.text = '*The MOTD either does not exist or could not be read*';
-					} else if (typeof status.description.text === 'undefined') {
-						status.description.text = '*The MOTD either does not exist or could not be read*';
-					}
-				} catch {
-					console.log(err);
-				}
-			}
-		});
-		timeout();
-	}, 10000);
-}
-timeout();
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('serverinfo')
-		.setDescription('Gives status information about a Minecraft server'),
+		.setDescription('Gives status information about Realmplex'),
 	async execute(interaction) {
+		let status;
+		let buffer;
+		const ip = 'realmplex.toaster.pw';
+		const port = '25569';
+		const server = new mcping.MinecraftServer(ip, port);
+
+		await new Promise((resolve, reject) => {
+			server.ping(10000, 760, (err, res) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					status = res;
+					const image = status.favicon.replace('data:image/png;base64,', '');
+					buffer = Buffer.from(image, 'base64');
+					if (status.previewsChat == undefined) {
+						status.previewsChat = 'false';
+					}
+					if (status.enforcesSecureChat == undefined) {
+						status.enforcesSecureChat = 'false';
+					}
+					resolve();
+				}
+			});
+		});
+
 		const attachment = new AttachmentBuilder(buffer, { name: 'icon.png' });
 		await interaction.reply({
 			embeds: [
@@ -50,9 +39,9 @@ module.exports = {
 					.setTitle('Minecraft Server Information')
 					.setTimestamp(Date.now())
 					.setThumbnail('attachment://icon.png')
-					.setDescription(ip)
+					.setDescription(`IP: ${ip}\nPort: ${port}`)
 					.addFields(
-						{ name: 'Information', value: `Version: ${status.version.name}\nPlayers: ${status.players.online}/${status.players.max}\nMOTD: ${status.description.text}` },
+						{ name: 'Information', value: `Version: ${status.version.name}\nPlayers: ${status.players.online}/${status.players.max}\nMOTD: Celebrating 2 Years!` },
 						{ name: 'Extra', value: `Previews Chat: ${status.previewsChat}\nEnforce Secure Profile: ${status.enforcesSecureChat}` },
 					),
 			],
