775f9174b6c619d6c72addf15ea3aead41f6b0cf
Ada <71234319+toasterada@users.noreply.github.com>
2023-06-01 10:39:40 +1200

    feat: owner check and error handling

--- src/commands/message.js
+++ src/commands/message.js
@@ -1,12 +1,15 @@
 const { ModalBuilder } = require('@discordjs/builders');
-const { SlashCommandBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('message')
-		.setDescription('Send a message')
-		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+		.setDescription('Send a message'),
 	async execute(interaction) {
+		if (interaction.user.id !== '616469681678581781') {
+			interaction.reply({ content: 'Only my owner may use this command!', ephemeral: true });
+			return;
+		}
 		const modal = new ModalBuilder()
 			.setCustomId('messageModal')
 			.setTitle('Send a message');
--- src/events/message.js
+++ src/events/message.js
@@ -4,9 +4,18 @@ module.exports = {
 	name: Events.InteractionCreate,
 	execute(interaction) {
 		if (!interaction.isModalSubmit()) return;
+		let embedObject;
 		const embedString = interaction.fields.components[0].components[0].value;
-		const embedObject = JSON.parse(embedString);
+
+		try {
+			embedObject = JSON.parse(embedString);
+		} catch {
+			interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			return;
+		}
+
 		interaction.channel.send(embedObject);
+
 		interaction.reply({ content: 'Message sent!', ephemeral: true });
 	},
 };
