06976861f0898e2e043233087bb9454ddfe14648
toasterw <71234319+toasterw@users.noreply.github.com>
2023-07-01 13:43:58 +1200

    feat: add channel option to /message (untested)

--- src/commands/message.js
+++ src/commands/message.js
@@ -1,10 +1,14 @@
 const { ModalBuilder } = require('@discordjs/builders');
-const { SlashCommandBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, Events } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('message')
-		.setDescription('Send a message'),
+		.setDescription('Send a message')
+		.addChannelOption(option => 
+			option.setName = ('channel')
+				.setDescription('The channel to send the message in.')
+				.setRequired(true)),
 	async execute(interaction) {
 		if (interaction.user.id !== '616469681678581781') {
 			interaction.reply({ content: 'Only my owner may use this command!', ephemeral: true });
@@ -24,5 +28,24 @@ module.exports = {
 		modal.addComponents(firstInput);
 
 		await interaction.showModal(modal);
+	},
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isModalSubmit() && interaction.customId !== 'messageModal') return;
+
+		let embedObject;
+		const embedString = interaction.fields.components[0].components[0].value;
+
+		try {
+			embedObject = JSON.parse(embedString);
+		} catch (err) {
+			console.error(err);
+			interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			return;
+		}
+
+		interaction.channel.send(embedObject);
+
+		await interaction.reply({ content: 'Message sent!', ephemeral: true });
 	},
 };
