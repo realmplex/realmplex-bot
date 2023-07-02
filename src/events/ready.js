25d017f3926a9e216567a2d37d61f9a2abdd2e8f
Ada <71234319+toasterw@users.noreply.github.com>
2023-07-02 23:19:38 +1200

    fix: remove playercount channel code

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,4 @@
 const { Events } = require('discord.js');
-const mcping = require('mcping-js');
 
 module.exports = {
 	name: Events.ClientReady,
@@ -7,24 +6,5 @@ module.exports = {
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		client.user.setPresence({ activities: [{ name: 'on Realmplex! realmplex.com' }], status: 'online' });
-		function timeout() {
-			setTimeout(async () => {
-				const server = new mcping.MinecraftServer('mc.realmplex.com', 25565);
-				server.ping(10000, 760, (err, res) => {
-					if (err) {
-						client.channels.fetch('1114356323056889937')
-							.then((channel) => channel.edit({ name: 'Server offline!' }));
-					} else {
-						const status = res;
-						client.channels.fetch('1114356323056889937')
-							.then((channel) => channel.edit({ name: `Online: ${status.players.online}/${status.players.max}` }));
-
-					}
-				});
-				timeout();
-			}, 600000);
-		}
-
-		timeout();
 	},
 };
