afe59d7bf5aade471419327196e6d0253fa8b20e
toasterw <71234319+toasterw@users.noreply.github.com>
2023-08-17 10:11:51 +1200

    refactor: roles command makes more sense

--- src/commands/roles.js
+++ src/commands/roles.js
@@ -1,11 +1,10 @@
 const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('@discordjs/builders');
-const { SlashCommandBuilder, ButtonStyle, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roles')
-		.setDescription('Get roles you need')
-		.setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+		.setDescription('Get roles you need'),
 	async execute(interaction) {
 		const roleButtons = new ActionRowBuilder()
 			.addComponents(
@@ -22,17 +21,20 @@ module.exports = {
 					.setLabel('Clear')
 					.setStyle(ButtonStyle.Danger),
 			);
+
 		const embed = new EmbedBuilder()
 			.setColor(0x4275f5)
 			.setTimestamp(Date.now())
 			.setTitle('Get roles here!')
 			.setDescription('Click on one of the buttons to receive/remove a role.\nThe clear button will remove all selectable roles.')
 			.setFooter({ text: 'Realmplex', iconURL: 'https://cdn.discordapp.com/avatars/1001311496036429845/82d48625a3789042b13c1e8053e64414.png' });
-		if (interaction.user.id !== '616469681678581781') {
-			await interaction.reply({ components: [roleButtons], embeds: [embed], ephemeral: true });
+
+		if (interaction.user.id === '616469681678581781') {
+			await interaction.reply({ content: 'Sending embed...', ephemeral: true });
+			await interaction.channel.send({ components: [roleButtons], embeds: [embed], ephemeral: false });
 			return;
+		} else {
+			await interaction.reply({ components: [roleButtons], embeds: [embed], ephemeral: true });
 		}
-		await interaction.reply({ content: 'Sending embed...', ephemeral: true });
-		await interaction.channel.send({ components: [roleButtons], embeds: [embed], ephemeral: false });
 	},
 };
