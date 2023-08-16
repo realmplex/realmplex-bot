5a4aa252d0ea7e5dd995e470a9c0d0d536f50f90
Ada <71234319+toasterw@users.noreply.github.com>
2023-08-16 22:09:10 +1200

    fix: wrong channel id

--- src/events/roles.js
+++ src/events/roles.js
@@ -14,7 +14,7 @@ module.exports = {
 					await member.roles.remove('902717103277244426');
 					return interaction.reply({ content: 'Successfully removed \'Server Announcements\'', ephemeral: true });
 				} catch (err) {
-					const channel = await interaction.client.channels.fetch('1001320296655102084');
+					const channel = await interaction.client.channels.fetch('817166710309388319');
 					channel.send({ embeds: [{
 						'title': `${err}`,
 						'description': 'An error occurred trying to modify the roles of a user.\nPlease check that my permissions are correct.',
@@ -27,7 +27,7 @@ module.exports = {
 					await member.roles.add('902717103277244426');
 					return interaction.reply({ content: 'Successfully added \'Server Announcements\'', ephemeral: true });
 				} catch (err) {
-					const channel = await interaction.client.channels.fetch('1001320296655102084');
+					const channel = await interaction.client.channels.fetch('817166710309388319');
 					channel.send({ embeds: [{
 						'title': `${err}`,
 						'description': 'An error occurred trying to modify the roles of a user.\nPlease check that my permissions are correct.',
@@ -44,7 +44,7 @@ module.exports = {
 					await member.roles.remove('947721012039012402');
 					return interaction.reply({ content: 'Successfully removed \'Media Notifications\'', ephemeral: true });
 				} catch (err) {
-					const channel = await interaction.client.channels.fetch('1001320296655102084');
+					const channel = await interaction.client.channels.fetch('817166710309388319');
 					channel.send({ embeds: [{
 						'title': `${err}`,
 						'description': 'An error occurred trying to modify the roles of a user.\nPlease check that my permissions are correct.',
@@ -57,7 +57,7 @@ module.exports = {
 					await member.roles.add('947721012039012402');
 					return interaction.reply({ content: 'Successfully added \'Media Notifications\'', ephemeral: true });
 				} catch (err) {
-					const channel = await interaction.client.channels.fetch('1001320296655102084');
+					const channel = await interaction.client.channels.fetch('817166710309388319');
 					channel.send({ embeds: [{
 						'title': `${err}`,
 						'description': 'An error occurred trying to modify the roles of a user.\nPlease check that my permissions are correct.',
@@ -72,7 +72,7 @@ module.exports = {
 				await interaction.member.roles.remove(allRoles);
 				return interaction.reply({ content: 'Successfully removed all selectable roles', ephemeral: true });
 			} catch (err) {
-				const channel = await interaction.client.channels.fetch('1001320296655102084');
+				const channel = await interaction.client.channels.fetch('817166710309388319');
 				channel.send({ embeds: [{
 					'title': `${err}`,
 					'description': 'An error occurred trying to modify the roles of a user.\nPlease check that my permissions are correct.',
